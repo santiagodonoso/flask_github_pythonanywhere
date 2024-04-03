24603f6245ba3012cb9fb6f49f1a91e2352b07c4
santiagodonoso <santiagodonoso@gmail.com>
2024-04-03 18:48:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import git
 
 ##############################
 
-# https://---GITHUB_TOKEN_HERE---@github.com/---GITHUB_ACCOUNT_NAME---/---GITHUB_REPOSITORY_NAME---.git
+# https://---GITHUB_TOKEN_HERE---@github.com/---GITHUB_USERNAME_NAME---/mysite.git
 
 app = Flask(__name__)
 
-@app.post('/---SECRET_URL_HERE---')
+@app.post('/SECRET-URL-HERE')
 def git_update():
-  repo = git.Repo('./---GITHUB_REPOSITORY_NAME---')
+  repo = git.Repo('./mysite')
   origin = repo.remotes.origin
   repo.create_head('main', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
   origin.pull()
